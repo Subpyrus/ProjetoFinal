4d38ff0ef4102da40ccf7c169ae048cb691c38b3
Diogo Gouveia <diogoxambre.gouveia@gmail.com>
2020-04-14 22:05:54 +0100

    commit

--- projeto_final/src/App.js
+++ projeto_final/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
-        <NavB/>
+      <NavB/>
         <Switch>
             <Route exact path="/" component={Dashboard}/>
         </Switch>
