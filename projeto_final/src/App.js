b9fbd66938c1c316271473916ab7040d33d6917c
João Rangel <Jrangel29@ua.pt>
2020-04-18 22:37:24 +0100

    Vendas

--- projeto_final/src/App.js
+++ projeto_final/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Dashboard from "./components/main_pages/Dashboard";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavB from './components/layout/Navbar';
+import Vendas from './components/main_pages/Venda';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <NavB/>
         <Switch>
             <Route exact path="/" component={Dashboard}/>
+            <Route path="/vendas" component={Vendas}/>
         </Switch>
     </BrowserRouter>
   );
---
+++ projeto_final/src/components/main_pages/Venda.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Img_Vendas from '../../Imgs/C_Vendas.png';
+import '../../App.css';
+
+class Vendas extends React.Component {
+    render() {
+        return (
+            <div className="row col-12 Body_Vendas">
+                <div className="col-md-5 offset-1">
+                    <img src={Img_Vendas} className="Img_Vendas"/>
+                </div>
+                <div className="col-md-5 Texto_Vendas">
+                    <h1 className="Titulo_Vendas">Bzzz!</h1>
+                    <h2 className="Subtitulo_Vendas ml-1">Esta funcionalidade não está disponível!</h2>
+                    <p className="Texto_Vendas_2 ml-1">É uma ideia muito fixe não é? Estamos a trabalhar para que num futuro próximo, possas colocar
+                        trabalhos que possam ser vendidos (ex: <i>templates, mockups,</i> produtos originais, etc).
+                        <br/>
+                        Vamos oferecer-te um espaço para rentabilizares o teu trabalho!</p>
+                </div>
+            </div>
+        )
+    }
+
+}
+
+export default Vendas;
