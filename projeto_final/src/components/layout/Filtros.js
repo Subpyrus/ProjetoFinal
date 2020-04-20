a42fe7f4586bfc492bce54a33863a74687f928dd
João Rangel <Jrangel29@ua.pt>
2020-04-20 21:12:16 +0100

    projetos - 2

--- projeto_final/src/components/layout/Filtros.js
+++ projeto_final/src/components/layout/Filtros.js
@@ -3,7 +3,7 @@ import '../../App.css';
 
 function Filtros() {
     return(
-        <div className="filtros container row col-12">
+        <div className="filtros row col-12 m-0 p-0">
             <span className="col-2 item_Filtro"/>
             <span className="col-2 item_Filtro">Área de trabalho</span>
             <span className="col-2 item_Filtro">Localização</span>
