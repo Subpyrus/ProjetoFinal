e407562359e4a3e25df654e6ba014ab442daa3ee
Diogo Gouveia <diogoxambre.gouveia@gmail.com>
2020-04-21 10:08:10 +0100

    commit

--- projeto_final/src/components/layout/SignedOutLinks.js
+++ projeto_final/src/components/layout/SignedOutLinks.js
@@ -4,12 +4,12 @@ import {NavLink} from 'react-router-dom';
 const SignedOutLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink className="links" to='/'>PROJETOS</NavLink></li>
+            <li><NavLink className="links" to='/projetos'>PROJETOS</NavLink></li>
             <li><NavLink className="links" to='/'>CRIADORES</NavLink></li>
             <li><NavLink className="links" to='/'>FREELANCE</NavLink></li>
             <li><NavLink className="links" to='/'>EMPREGOS</NavLink></li>
-            <li><NavLink className="links" to='/'>VENDAS</NavLink></li>
-            <li><NavLink className="links" to='/'>ENTRAR</NavLink></li>
+            <li><NavLink className="links" to='/vendas'>VENDAS</NavLink></li>
+            <li><NavLink className="links" to='/entrar'>ENTRAR</NavLink></li>
         </ul>
     )
 }
