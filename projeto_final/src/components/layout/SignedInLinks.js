69366790b138cb1a9d181c53d3e4671abf3c3850
Diogo Gouveia <diogoxambre.gouveia@gmail.com>
2020-04-21 10:14:01 +0100

    commit

--- projeto_final/src/components/layout/SignedInLinks.js
+++ projeto_final/src/components/layout/SignedInLinks.js
@@ -4,11 +4,11 @@ import {NavLink} from 'react-router-dom';
 const SignedInLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink className="links" to='/'>PROJETOS</NavLink></li>
+            <li><NavLink className="links" to='/projetos'>PROJETOS</NavLink></li>
             <li><NavLink className="links" to='/'>CRIADORES</NavLink></li>
             <li><NavLink className="links" to='/'>FREEELANCE</NavLink></li>
             <li><NavLink className="links" to='/'>EMPREGOS</NavLink></li>
-            <li><NavLink className="links" to='/'>VENDAS</NavLink></li>
+            <li><NavLink className="links" to='/vendas'>VENDAS</NavLink></li>
             <li><NavLink to='/' className="btn-small btn-floating yellow"></NavLink></li>
         </ul> 
     )
