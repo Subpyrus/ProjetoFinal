07a1cfe279555317395548d836ecbea13c5318cf
João Rangel <Jrangel29@ua.pt>
2020-04-22 23:31:10 +0100

    projetos - 4

--- projeto_final/src/components/layout/Filtros.js
+++ projeto_final/src/components/layout/Filtros.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import '../../App.css';
 
-function Filtros() {
-    return(
-        <div className="filtros row col-12 m-0 p-0">
-            <span className="col-2 item_Filtro"/>
-            <span className="col-2 item_Filtro">Área de trabalho</span>
-            <span className="col-2 item_Filtro">Localização</span>
-            <span className="col-2 item_Filtro">Destaques</span>
-            <span className="col-2 item_Filtro">Pesquisa</span>
-            <span className="col-2 item_Filtro"/>
-        </div>
-    )
+class Filtros extends React.Component {
+    render() {
+        return (
+            <div className="filtros row col-12 m-0 p-0">
+                <span className="col-2"/>
+                <span className="col-2 item_Filtro">Área de Trabalho</span>
+                <span className="col-2 item_Filtro">Localização</span>
+                <span className="col-2 item_Filtro">Destaques</span>
+                <span className="col-2 item_Filtro2">Pesquisa</span>
+                <span className="col-2"/>
+            </div>
+        )
+    }
 }
 
 export default Filtros
--- projeto_final/src/components/main_pages/Projeto.js
+++ projeto_final/src/components/main_pages/Projeto.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../../App.css';
 import Filtros from '../layout/Filtros';
+/* alterar isto para abrir outra página */
+import {Modal, Button} from 'react-bootstrap'
 
 class Projetos extends React.Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            setShow: false,
+        }
+    }
+
+    handleClose = () => {
+        this.setState({setShow: false})
+    };
+    handleShow = () => {
+        console.log("oi");
+        this.setState({setShow: true})
+    };
+
     render() {
         return (
             <div className="container-fluid m-0 p-0 Proj_Body">
                 <Filtros/>
                 <div className="row col-12 justify-content-center mt-5">
-                    <div className="col-2 mr-2">
+                    <div className="col-2 mr-2" onClick={this.handleShow}>
                         <div className="card m-0 p-0">
                             <div className="card-body">
                                 <h3 className="card-title Proj_Info">Maria betrolina</h3>
@@ -45,6 +62,23 @@ class Projetos extends React.Component {
                         </div>
                     </div>
                 </div>
+
+                /* esta parte é só para as fases*/
+                <Modal show={this.state.setShow} onHide={this.handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={this.handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={this.handleClose}>
+                            Save Changes
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+
             </div>
         )
     }
