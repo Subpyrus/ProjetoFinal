b5eab88be3ecd42de9cbab5ce97761cb08a19f8e
Pedro <pedrosantos00@ua.pt>
2020-06-22 15:15:58 +0100

    dashboard-responsive-seccões+animações-botões venda-responsive css-muita-coisa

--- projeto_final/src/components/main_pages/Dashboard.js
+++ projeto_final/src/components/main_pages/Dashboard.js
@@ -11,68 +11,70 @@ import {Link} from 'react-router-dom'
 class Pag_Principal extends React.Component {
     render() {
         return (
-            <div className="pagP container-fluid ml-lg-0 pl-lg-0">
-                <div className="inicial row justify-content-center">
-                    <span className="col-lg-4 text-sm-center pr-0 mr-0">
+            <div className="pagP container-fluid m-0 p-0">
+                <div className="inicial row m-0 justify-content-center">
+                    <span className="col-4 logoimg text-sm-center">
                         <img src={Logo} width="auto" height="300px" className="P_Ini_Logo pl-4"/>
                     </span>
-                    <div className="col-lg-6 pl-5 text-sm-center text-lg-left pl-lg-0 ml-lg-0 justify-content-sm-center justify-content-lg-start">
+                    <div className="col-6 col-xs-12 text-sm-center text-lg-left justify-content-sm-center justify-content-lg-start">
                         <span className="P_Ini_texto">Bem Vindos à Colmeia de Criadores Artísticos</span>
                         <Link to="/registo">
-                            <button className="P_Ini_button">Criar Perfil</button>
+                            <button className="P_Ini_button" data-hover="Junta-te a Nós!">
+                                <div>Criar Perfil</div>
+                            </button>
                         </Link>
                     </div>
                 </div>
-                <div className="conteudo1 row">
-                    <div className="texto col-lg-6">
-                        <h1>Projetos</h1>
-                        <p className="Texto_P_Princ">Projetos é a montra de trabalhos, de todo o teu portfólio. A Hinamic oferece-te a melhor
-                            oportunidade para promoveres o teu trabalho. Não percas mais tempo e partilha as tuas
-                            criações para ganhares notoriedade!</p>
-                        <Link to="/projetos">
-                            <button className="P_Ini_button_P">Projetos</button>
-                        </Link>
+                    <div className="row conteudo1 m-0">
+                        <div className="texto col-md-5 col-sm-12">
+                            <h1 className="font-weight-bold">Projetos</h1>
+                            <p className="Texto_P_Princ">Projetos é a montra de trabalhos, de todo o teu portfólio. A Hinamic oferece-te a melhor
+                                oportunidade para promoveres o teu trabalho. Não percas mais tempo e partilha as tuas
+                                criações para ganhares notoriedade!</p>
+                            <Link to="/projetos">
+                                <button className="P_Ini_button_P">Ver Projetos</button>
+                            </Link>
+                        </div>
+                        <div className="text-center mr-4 col-md-5 d-none d-md-block">
+                            <img className="Cartoon" src={C_Projetos} width="290px" height="290px"/>
+                        </div>
                     </div>
-                    <div className="col-lg-5">
-                        <img className="Cartoon" src={C_Projetos} width="350px" height="350px"/>
+                <div className="conteudo2 row m-0">
+                    <div className="text-center col-md-5 d-none d-md-block">
+                        <img className="Cartoon2" src={C_Criadores} width="290px" height="290px"/>
                     </div>
-                </div>
-                <div className="conteudo2 row">
-                    <div className="col-lg-5">
-                        <img className="Cartoon2" src={C_Criadores} width="350px" height="350px"/>
-                    </div>
-                    <div className="texto2 col-lg-6">
-                        <h1>Criadores</h1>
+                    <div className="texto2 col-md-5 col-sm-12">
+                        <h1 className="font-weight-bold">Criadores</h1>
                         <p className="Texto_P_Princ">Aqui encontrarás Criadores de diferentes áreas: Design, Música, Programação, Cinema,
                             Fotografia, entre outras. Quem sabe se não encontras por aqui um parceiro de trabalho!</p>
                         <Link to="/criadores">
-                            <button className="P_Ini_button_P">Criadores</button>
+                            <button className="P_Ini_button_P">Ver Criadores</button>
                         </Link>
                     </div>
                 </div>
-                <div className="conteudo3 row">
-                    <div className="texto3 col-lg-6">
-                        <h1>Freelance</h1>
-                        <p className="Texto_P_Princ">As abelhas trabalham em conjunto na sua colmeia. Na Hinamic incentivamos o associativismo
-                            entre Criadores. Quem sabe não terás a solução para outro Criador?</p>
-                        <Link to="/freelance">
-                            <button className="P_Ini_button_P">Freelance</button>
-                        </Link>
+                    <div className="conteudo1 row m-0">
+                        <div className="texto col-md-5 col-sm-12">
+                            <h1 className="font-weight-bold">Freelance</h1>
+                            <p className="Texto_P_Princ">As abelhas trabalham em conjunto na sua colmeia. Na Hinamic incentivamos o associativismo
+                                entre Criadores. Quem sabe não terás a solução para outro Criador?</p>
+                            <Link to="/freelance">
+                                <button className="P_Ini_button_P">Ver Freelancers</button>
+                            </Link>
+                        </div>
+                        <div className="text-center mr-4 col-md-5 d-none d-md-block">
+                            <img className="Cartoon" src={C_Freelance} width="290px" height="290px"/>
+                        </div>
                     </div>
-                    <div className="col-lg-5">
-                        <img className="Cartoon3" src={C_Freelance} width="350px" height="350px"/>
+                <div className="conteudo2 row m-0">
+                    <div className="text-center ol-md-5 d-none d-md-block">
+                        <img className="Cartoon2" src={C_Empregos} width="290px" height="290px"/>
                     </div>
-                </div>
-                <div className="conteudo4 row">
-                    <div className="col-lg-5">
-                        <img className="Cartoon4" src={C_Empregos} width="350px" height="350px"/>
-                    </div>
-                    <div className="texto4 col-lg-6">
-                        <h1>Empregos</h1>
+                    <div className="texto2 col-md-5 col-sm-12">
+                        <h1 className="font-weight-bold">Emprego</h1>
                         <p className="Texto_P_Princ">Para vos ajudar a construir o futuro podem procurar aqui
                             ofertas de trabalho, em diferentes empresas. O vosso futuro pode estar aqui!</p>
                         <Link to="/empregos">
-                            <button className="P_Ini_button_P">Empregos</button>
+                            <button className="P_Ini_button_P">Ver Empregos</button>
                         </Link>
                     </div>
                 </div>
--- projeto_final/src/components/main_pages/Venda.js
+++ projeto_final/src/components/main_pages/Venda.js
@@ -5,19 +5,21 @@ import '../../App.css';
 class Vendas extends React.Component {
     render() {
         return (
-            <div className="container-fluid row col-12 Body_Vendas">
-                <div className="col-md-5 offset-1">
-                    <img src={Img_Vendas} className="Img_Vendas"/>
+            <div className="container-fluid Body_Vendas">
+                <div className="row m-0 cenas p-0 justify-content-center">
+                    <div className="text-center col-md-6 d-none d-md-block">
+                        <img src={Img_Vendas} className="Img_Vendas m-0"  width="350px" height="350px"/>
+                    </div>
+                    <div className="col-md-6 col-sm-12 Texto_Vendas">
+                        <h1 className="Titulo_Vendas">Bzzz!</h1>
+                        <h2 className="Subtitulo_Vendas ml-1">Esta funcionalidade não está disponível!</h2>
+                        <p className="Texto_Vendas_2 ml-1">É uma ideia muito fixe não é? Estamos a trabalhar para que
+                            num futuro próximo, possas colocar
+                            trabalhos que possam ser vendidos (ex: <i>templates, mockups,</i> produtos originais, etc).
+                            <br/>
+                            Vamos oferecer-te um espaço para rentabilizares o teu trabalho!</p>
+                    </div>
                 </div>
-                <div className="col-md-5 Texto_Vendas">
-                    <h1 className="Titulo_Vendas">Bzzz!</h1>
-                    <h2 className="Subtitulo_Vendas ml-1">Esta funcionalidade não está disponível!</h2>
-                    <p className="Texto_Vendas_2 ml-1">É uma ideia muito fixe não é? Estamos a trabalhar para que num futuro próximo, possas colocar
-                        trabalhos que possam ser vendidos (ex: <i>templates, mockups,</i> produtos originais, etc).
-                        <br/>
-                        Vamos oferecer-te um espaço para rentabilizares o teu trabalho!</p>
-                </div>
-                <span className="col-md-1"/>
             </div>
         )
     }
